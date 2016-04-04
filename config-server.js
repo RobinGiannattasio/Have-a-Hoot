b1d3d958ea697167c6ca82ee7a7df9487900921f
RobinGiannattasio <Robin.Giannattasio@gmail.com>
2016-04-04 18:45:01 -0400

    reconfig server

--- config-server.js
+++ config-server.js
@@ -9,8 +9,8 @@ app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/client'));
 
-// app.get('/', function(req,res){
-  
-// })
+app.get('/', function(req,res){
+  res.redirect('/index.html')
+})
 
 module.exports = app;
