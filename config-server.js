acc6b4388b9079afbddc55e9bcf9efef51a52a5e
RobinGiannattasio <Robin.Giannattasio@gmail.com>
2016-04-04 18:53:23 -0400

    reconfig server

--- config-server.js
+++ config-server.js
@@ -1,16 +1,16 @@
 var express = require('express');
-var partials = require('express-partials');
-var bodyParser = require('body-parser');
+// var partials = require('express-partials');
+// var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(partials());
-app.use(bodyParser.json());
+// app.use(partials());
+// app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/CLIENT'));
 
-app.get('/', function(req,res){
-  res.redirect('/index.html')
-})
+// app.get('/', function(req,res){
+
+// })
 
 module.exports = app;
